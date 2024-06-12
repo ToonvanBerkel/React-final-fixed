ae817560e52839ac7f3209a088a77e752cde59ee
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2024-06-12 16:10:10 +0200

    push

---
+++ welcome/greeting-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ welcome/greeting-app/src/WelcomeMessage.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const WelcomeMessage = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    let newMessage = '';
+
+    if (hours < 12) {
+      newMessage = 'Goedemorgen';
+    } else if (hours < 18) {
+      newMessage = 'Goedemiddag';
+    } else {
+      newMessage = 'Goedenavond';
+    }
+
+    setMessage(newMessage);
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
